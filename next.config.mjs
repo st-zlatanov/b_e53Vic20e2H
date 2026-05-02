00375ad37031e1fe641f046a2d513f1b1831e094
st-zlatanov <stiliyan.zlatanov@gmail.com>
2026-05-02 18:40:14 +0300

    final 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['be53vic204136.builtwithrocket.new'],
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
+
   images: {
     unoptimized: true,
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'autoworks-opbyyhr14-stiliyanzlatanov-3620s-projects.vercel.app',
+          },
+        ],
+        destination: 'https://autoworksbg.com/:path*',
+        permanent: true,
+      },
+    ]
   },
 }
 
